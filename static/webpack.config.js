089df65abfaef68e490eab6117b4e51db56044a4
oleob <ole.barsch@gmail.com>
2018-03-23 16:38:18 +0100

    change output path to static in server folder

--- static/webpack.config.js
+++ static/webpack.config.js
@@ -1,8 +1,11 @@
 const webpack = require('webpack');
+const path = require('path');
+
 const config = {
   entry:  __dirname + '/js/index.jsx',
   output: {
-      path: __dirname + '/dist',
+      //path: __dirname + '/dist',
+      path: path.join(__dirname,'..', 'server', 'static', 'js'),
       filename: 'bundle.js',
   },
   resolve: {
