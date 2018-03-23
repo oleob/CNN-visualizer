820e825a68d272379b6b7f92d64f53d2bc328a23
oleob <ole.barsch@gmail.com>
2018-03-23 16:40:16 +0100

    Change to material ui style

--- static/js/components/ImageForm.jsx
+++ static/js/components/ImageForm.jsx
@@ -1,28 +1,43 @@
 import React, { Component } from 'react';
+import Button from 'material-ui/RaisedButton';
+
 import {predict} from '../utilities/apiCalls';
+import Scoreboard from './Scoreboard';
 
 class ImageForm extends Component {
-    constructor(props) {
-      super(props)
-      this.uploadFile = this.uploadFile.bind(this);
+  constructor(props) {
+    super(props)
+
+    this.state={
+      results: [],
     }
 
-    uploadFile(event) {
-        let file = event.target.files[0];
-        if (file) {
-          let data = new FormData();
-          data.append('image', file);
-          predict(data)
-        }
-    }
+    this.uploadFile = this.uploadFile.bind(this);
+  }
 
-    render() {
-      return <span>
-        <input type="file"
-        name="myFile"
-        onChange={this.uploadFile} />
-      </span>
-    }
+  uploadFile(event) {
+      let file = event.target.files[0];
+      if (file) {
+        let data = new FormData();
+        data.append('image', file);
+        predict(data).then((results)=>{
+          this.setState({
+            results,
+          })
+        })
+      }
+  }
+
+  render() {
+    return (
+      <div className = "imageForm">
+        <Button containerElement='label' label='Upload file'>
+          <input type="file" name="myFile" onChange={this.uploadFile}  style={{"display" : "none"}}/>
+        </Button>
+        <Scoreboard results={this.state.results} />
+      </div>
+    )
+  }
 }
 
 export default ImageForm
