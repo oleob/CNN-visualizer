cced0b4860a8d8e5a4168e09db4e4d41de042784
oleob <ole.barsch@gmail.com>
2018-03-23 17:15:44 +0100

    Add activations

--- static/js/components/Layer.jsx
+++ static/js/components/Layer.jsx
@@ -1,9 +1,46 @@
 import React, { Component } from 'react';
 
-const Layer = (props) => (
-  <div className="layer">
-    <p style={{margin:0}} >placeholder</p>
-  </div>
-);
+import { activations } from '../utilities/apiCalls';
+
+import Activations from './Activations';
+
+class Layer extends Component {
+
+  constructor(){
+    super();
+
+    this.state = {
+      showActivations: false,
+      filepaths: [],
+    }
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(){
+    if(!this.state.showActivations){
+      activations().then((res) => {
+        this.setState({
+          showActivations: true,
+          filepaths: res.filepaths,
+        });
+      })
+    } else {
+      this.setState({
+        showActivations: !this.state.showActivations,
+      });
+    }
+  }
+
+  render(){
+    return (
+      <div className="layer" onClick={this.handleClick}>
+        <Activations showActivations={this.state.showActivations} filepaths={this.state.filepaths}/>
+        <p style={{margin:0}}>placeholder</p>
+      </div>
+    );
+  }
+}
+
 
 export default Layer;
--- static/js/utilities/apiCalls.js
+++ static/js/utilities/apiCalls.js
@@ -1,11 +1,19 @@
 import axios from 'axios';
 
 const predict = (data) => {
-  return new Promise((resolve, reject)=>{
-    axios.post('/predict', data).then((res)=>{
+  return new Promise((resolve, reject) => {
+    axios.post('/predict', data).then((res) => {
       resolve(res.data)
     })
   })
 }
 
-export {predict};
+const activations = (layerName) => {
+  return new Promise((resolve, reject) => {
+    axios.post('activations', {layerName}).then((res) => {
+      resolve(res.data);
+    })
+  })
+}
+
+export {predict, activations};
