ccb6656d306ce89144e00f2e231b38112ebed990
oleob <ole.barsch@gmail.com>
2018-03-23 16:40:00 +0100

    add endpoint for activations

--- server/server.py
+++ server/server.py
@@ -1,22 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, Response
 import utilities.network as net
 import cv2
 import numpy as np
 import json
 import io
 
-app = Flask(__name__, static_folder='../static/dist', template_folder='../static')
+app = Flask(__name__, static_folder='./static', template_folder='./static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# @app.route("/predict", methods=["POST"])
-# def predict():
-#     img = cv2.imread('./images/dog.jpg',1)
-#     prediction = net.predict(img)
-#     return json.dumps(prediction)
-
+@app.route('/activations', methods=['GET'])
+def test():
+    filepaths = net.get_layer_activations('mixed4e_5x5')
+    return json.dumps({'filepaths': filepaths})
 @app.route("/predict", methods=["POST"])
 def predict():
     image = request.files['image']
