7a4a75389fba4e64a4de9c00fcfef9ecb8bdbc51
oleob <ole.barsch@gmail.com>
2018-04-16 17:00:14 +0200

    fixed avg_pool bug

--- server/utilities/taylor.py
+++ server/utilities/taylor.py
@@ -9,57 +9,66 @@ class Taylor:
         self.sess = sess
         self.epsilon = epsilon
         self.graph = sess.graph
-        img = cv2.imread('./static/images/penguins3.jpg',1)
+        img = cv2.imread('./static/images/dog-horse.jpg',1)
         self.img = cv2.resize(img, (224,224))
+        self.output_layer = sess.graph.get_tensor_by_name('output2:0')
+        self.new_output = tf.placeholder(tf.float32, self.output_layer.shape)
 
-    def __call__(self, output_layer):
+    def __call__(self):
         #get values of output_layer
-        units = self.sess.run(output_layer, feed_dict={"input:0":[self.img]})[0]
         # get softmax_gradients
-        output_node = tf.slice(output_layer, [0, np.argmax(units)], [1, 1])
-        relevance = [output_node]
-        layer = output_layer.op.inputs[0]
+        #output_node = tf.slice(output_layer, [0, np.argmax(units)], [1, 1])
+        #print(self.sess.run(output, feed_dict={"input:0":[self.img]})[0])
+        relevance = [self.new_output]
+        #relevance = [self.output_layer]
+        layer = self.output_layer.op.inputs[0]
         while not (layer.op.name=='input'):
-            if 'softmax' in layer.op.name:
-                weights = self.graph.get_tensor_by_name(layer.op.name + '_w:0')
-                activation = self.get_parent(layer, 3)
-                relevance.append(self.backprop_dense(activation, weights, relevance[-1]))
-                print(layer.op.name, activation.op.name)
-                layer = activation
-            elif 'avgpool' in layer.op.name:
-                activation = self.get_parent(layer, 2)
-                relevance.append(self.backprop_avg_pool(activation, relevance[-1]))
-                layer = activation
-            elif 'mixed' in layer.op.name:
-                print(layer.op.name, activation.op.name)
-                activation = self.get_parent(layer, 4)
-                relevance.append(self.backprop_inception(activation, layer, relevance[-1]))
-                #print(layer.op.name, activation.op.name)
-                layer = activation
-            elif 'maxpool' in layer.op.name:
-                activation = self.get_parent(layer, 1)
-                relevance.append(self.backprop_max_pool(activation, relevance[-1]))
-                print(layer.op.name, activation.op.name)
-                layer = activation
-            elif 'localresponsenorm' in layer.op.name:
-                layer = self.get_parent(layer, 1)
-            elif 'conv2d0' in layer.op.name:
-                weights = self.graph.get_tensor_by_name(layer.op.name + '_w:0')
-                activation = self.get_parent(layer, 3)
-                print(layer.op.name, activation.op.name)
-                relevance.append(self.backprop_conv(activation, weights, relevance[-1], strides=[1, 2, 2, 1]))
-                layer = activation
-            elif 'conv' in layer.op.name:
-                weights = self.graph.get_tensor_by_name(layer.op.name + '_w:0')
-                activation = self.get_parent(layer, 3)
-                print(layer.op.name, layer.shape, activation.op.name)
-                relevance.append(self.backprop_conv(activation, weights, relevance[-1]))
-                layer = activation
-            else:
-                print(layer.shape, [inp.op for inp in layer.op.inputs if not (inp.op.type=='Const')])
-                break
+            with tf.variable_scope(layer.op.name + '_taylor'):
+                if 'softmax' in layer.op.name:
+                    weights = self.graph.get_tensor_by_name(layer.op.name + '_w:0')
+                    activation = self.get_parent(layer, 3)
+                    relevance.extend(self.backprop_dense(activation, weights, relevance[-1]))
+                    print(layer.op.name, activation.op.name)
+                    layer = activation
+                    #break
+                elif 'avgpool0/reshape' in layer.op.name:
+                    relevance.append(tf.reshape(relevance[-1], (-1, 1, 1, 1024)))
+                    layer = self.get_parent(layer, 1)
+                elif 'avgpool' in layer.op.name:
+                    activation = self.get_parent(layer, 1)
+                    relevance.append(self.backprop_avg_pool(activation, relevance[-1]))
+                    layer = activation
+                elif 'mixed' in layer.op.name:
+                    print(layer.op.name, activation.op.name)
+                    activation = self.get_parent(layer, 4)
+                    relevance.append(self.backprop_inception(activation, layer, relevance[-1]))
+                    #print(layer.op.name, activation.op.name)
+                    layer = activation
+                elif 'maxpool' in layer.op.name:
+                    activation = self.get_parent(layer, 1)
+                    relevance.append(self.backprop_max_pool(activation, relevance[-1]))
+                    print(layer.op.name, activation.op.name)
+                    layer = activation
+                elif 'localresponsenorm' in layer.op.name:
+                    layer = self.get_parent(layer, 1)
+                elif 'conv2d0' in layer.op.name:
+                    weights = self.graph.get_tensor_by_name(layer.op.name + '_w:0')
+                    activation = self.get_parent(layer, 3)
+                    print(layer.op.name, activation.op.name)
+                    relevance.append(self.backprop_conv(activation, weights, relevance[-1], strides=[1, 2, 2, 1]))
+                    #relevance.append(self.backprop_conv_input(activation, weights, relevance[-1]))
+                    layer = activation
+                elif 'conv' in layer.op.name:
+                    weights = self.graph.get_tensor_by_name(layer.op.name + '_w:0')
+                    activation = self.get_parent(layer, 3)
+                    print(layer.op.name, activation.op.name)
+                    relevance.append(self.backprop_conv(activation, weights, relevance[-1]))
+                    layer = activation
+                else:
+                    print(layer.shape, [inp.op for inp in layer.op.inputs if not (inp.op.type=='Const')])
+                    break
 
-        return relevance[-1]
+        return relevance
 
     def get_parent(self, child, num_skips):
         parent = child
@@ -77,7 +86,7 @@ class Taylor:
         r_3x3 = self.backprop_3x3(activation, name, r_3)
         r_5x5 = self.backprop_5x5(activation, name, r_5)
         r_pool = self.backprop_inception_max_pool(activation, name, r_p)
-        new_relevance = r_1x1 + r_3x3 + r_5x5 + r_pool
+        new_relevance = (r_1x1 + r_3x3 + r_5x5 + r_pool)
         return new_relevance
 
     def backprop_dense(self, activation, weights, relevance):
@@ -85,7 +94,7 @@ class Taylor:
         z = tf.matmul(activation, w_pos) + self.epsilon
         s = relevance / z
         c = tf.matmul(s, tf.transpose(w_pos))
-        return c * activation
+        return [c * activation]
 
     def backprop_max_pool(self, activation, relevance, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1]):
         z = nn_ops.avg_pool(activation, ksize, strides, padding='SAME') + self.epsilon
@@ -101,14 +110,16 @@ class Taylor:
         new_relevance = self.backprop_conv(activation1, weights, relevance)
 
         z = tf.nn.max_pool(activation2, ksize, strides, padding='SAME') + self.epsilon
+        #z = nn_ops.avg_pool(activation2, ksize, strides, padding='SAME') + self.epsilon
         s = new_relevance / z
         c = gen_nn_ops.max_pool_grad_v2(activation2, z, s, ksize, strides, padding='SAME')
+        #c = gen_nn_ops._avg_pool_grad(tf.shape(activation2), s, ksize, strides, padding='SAME')
         return c * activation2
 
     def backprop_avg_pool(self, activation, relevance, ksize=[1, 7, 7, 1], strides=[1, 1, 1, 1]):
-        z = tf.nn.avg_pool(activation, ksize, strides, padding='SAME') + self.epsilon
+        z = tf.nn.avg_pool(activation, ksize, strides, padding='VALID') + self.epsilon
         s = relevance / z
-        c = gen_nn_ops._avg_pool_grad(tf.shape(activation), s, ksize, strides, padding='SAME')
+        c = gen_nn_ops._avg_pool_grad(tf.shape(activation), s, ksize, strides, padding='VALID')
         return c * activation
 
     def backprop_conv(self, activation, weights, relevance, strides=[1, 1, 1, 1], padding='SAME'):
@@ -142,21 +153,50 @@ class Taylor:
         new_relevance = self.backprop_conv(activation1, weights1, relevance)
         return self.backprop_conv(activation2, weights2, new_relevance)
 
+    def backprop_conv_input(self, activation, weights, relevance, strides=[1, 2, 2, 1], padding='SAME', lowest=0., highest=1.):
+        W_p = tf.maximum(0., weights)
+        W_n = tf.minimum(0., weights)
+
+        L = tf.ones_like(activation, tf.float32) * lowest
+        H = tf.ones_like(activation, tf.float32) * highest
+
+        z_o = nn_ops.conv2d(activation, weights, strides, padding)
+        z_p = nn_ops.conv2d(L, W_p, strides, padding)
+        z_n = nn_ops.conv2d(H, W_n, strides, padding)
+
+        z = z_o - z_p - z_n + 1e-10
+        s = relevance / z
+
+        c_o = nn_ops.conv2d_backprop_input(tf.shape(activation), weights, s, strides, padding)
+        c_p = nn_ops.conv2d_backprop_input(tf.shape(activation), W_p, s, strides, padding)
+        c_n = nn_ops.conv2d_backprop_input(tf.shape(activation), W_n, s, strides, padding)
+
+        return activation * c_o - L * c_p - H * c_n
+
     def run(self, relevance):
 
-        res = self.sess.run(relevance, feed_dict={"input:0":[self.img]})
-        res = res[0,:,:,:]
-        res = res[:,:,0] + res[:,:,1] + res[:,:,2]
-        #for i in range(res.shape[2]):
-        acti = res
-        su = acti.sum()
-        acti = acti/(acti.max() + self.epsilon)
-        fig = plt.figure()
-        ax = fig.add_subplot(111)
-        ax.axis('off')
-        ax.imshow(acti, cmap='Reds', interpolation='bilinear')
+        output_values = np.zeros(int(self.output_layer.shape[1]))
+        units = self.sess.run(self.output_layer, feed_dict={"input:0":[self.img]})[0]
+        output_values[np.argmax(units)] = 1
 
-        fig.savefig('static/images/temp/{0}.jpg'.format(su))
+        result = self.sess.run(relevance, feed_dict={"input:0":[self.img], self.new_output: [output_values]})
+        for r in range(len(result)):
+            res = result[r]
+            if(len(res.shape) >= 4):
+                print(r, relevance[r].op.name, res.shape)
+                res = res[0,:,:,:]
+                #res = res[:,:,0] + res[:,:,1] + res[:,:,2]
+                res = np.sum(res, axis=2)
+                acti = res
+                acti /= acti.max()
+                fig = plt.figure()
+                ax = fig.add_subplot(111)
+                ax.axis('off')
+                ax.imshow(acti, cmap='Reds', interpolation='nearest')
+
+                fig.savefig('static/images/temp/{0}.jpg'.format(r))
+            else:
+                print('-', relevance[r].op.name, res)
             # acti = cv2.resize(acti, (224,224), interpolation=0)
             # r,g,b = cv2.split(self.img)
             # r = r*acti
@@ -167,8 +207,7 @@ class Taylor:
 
 def test(sess):
     taylor = Taylor(sess)
-    output_layer = sess.graph.get_tensor_by_name('output2:0') #TODO fix
-    relevance = taylor(output_layer)
+    relevance = taylor()
     taylor.run(relevance)
 
 def print_names():
