c4a499b1a0ce337be178ae89a193c7060c05e147
oleob <ole.barsch@gmail.com>
2018-04-19 09:49:45 +0200

    move traversing to seperate file

---
+++ server/utilities/net_funcs/inception_v1.py
@@ -0,0 +1,51 @@
+import tensorflow as tf
+
+def traverse_graph(self, layer, relevances):
+    while not ('ExpandDims' in layer.name):
+        print(layer.name)
+        with tf.variable_scope(layer.op.name + '_taylor'):
+            #if squeeze skip to next node
+            if 'SpatialSqueeze' in layer.name:
+                layer = self.get_parent(layer, 1)
+            #if BiasAdd = end of convolution
+            elif 'BiasAdd' in layer.name:
+                conv = self.get_parent(layer,1)
+                weights = self.get_parent(conv.op.inputs[1], 1)
+                activation = self.get_parent(layer, 3)
+                relevances.append(self.backprop_conv(activation, weights, relevances[-1], strides=conv.op.get_attr('strides')))
+                layer = activation
+            elif 'Dropout' in layer.name:
+                layer = self.get_parent(layer, 1)
+            elif 'AvgPool' in layer.name:
+                activation = self.get_parent(layer, 1)
+                ksize = layer.op.get_attr('ksize')
+                strides = layer.op.get_attr('strides')
+                padding = layer.op.get_attr('padding')
+                relevances.append(self.backprop_avg_pool(activation, relevances[-1], ksize, strides, padding))
+                layer = activation
+            elif 'concat' in layer.name:
+                activation = self.get_parent(layer, 4)
+                relevances.append(self.backprop_inception(layer, activation, relevances[-1]))
+                #print(layer.op.name, activation.op.name)
+                layer = activation
+            elif 'MaxPool' in layer.name:
+                activation = self.get_parent(layer, 1)
+                ksize = layer.op.get_attr('ksize')
+                strides = layer.op.get_attr('strides')
+                padding = layer.op.get_attr('padding')
+                relevances.append(self.backprop_avg_pool(activation, relevances[-1], ksize, strides, padding))
+                layer = activation
+            elif 'Relu' in layer.name:
+                activation = self.get_parent(layer, 3)
+                conv = self.get_parent(layer, 2)
+                weights = self.get_parent(conv.op.inputs[1], 1)
+                if not ('ExpandDims' in activation.name):
+                    relevances.append(self.backprop_conv(activation, weights, relevances[-1], strides=conv.op.get_attr('strides')))
+                else:
+                    relevances.append(self.backprop_conv_input(activation, weights, relevances[-1], strides=conv.op.get_attr('strides')))
+                layer = activation
+            else:
+                print('end:', layer.name)
+                break
+
+    return relevances
--- server/utilities/slim_taylor.py
+++ server/utilities/slim_taylor.py
@@ -3,6 +3,7 @@ import cv2
 from tensorflow.python.ops import nn_ops, gen_nn_ops
 import numpy as np
 import matplotlib.pyplot as plt
+from utilities.net_funcs.inception_v1 import traverse_graph
 
 class Taylor:
     def __init__(self, sess, epsilon=1e-10):
@@ -15,54 +16,7 @@ class Taylor:
     def __call__(self):
         relevances = [self.output_layer]
         layer = self.output_layer.op.inputs[0]
-        while not ('ExpandDims' in layer.name):
-            print(layer.name)
-            with tf.variable_scope(layer.op.name + '_taylor'):
-                #if squeeze skip to next node
-                if 'SpatialSqueeze' in layer.name:
-                    layer = self.get_parent(layer, 1)
-                #if BiasAdd = end of convolution
-                elif 'BiasAdd' in layer.name:
-                    conv = self.get_parent(layer,1)
-                    weights = self.get_parent(conv.op.inputs[1], 1)
-                    activation = self.get_parent(layer, 3)
-                    relevances.append(self.backprop_conv(activation, weights, relevances[-1], strides=conv.op.get_attr('strides')))
-                    layer = activation
-                elif 'Dropout' in layer.name:
-                    layer = self.get_parent(layer, 1)
-                elif 'AvgPool' in layer.name:
-                    activation = self.get_parent(layer, 1)
-                    ksize = layer.op.get_attr('ksize')
-                    strides = layer.op.get_attr('strides')
-                    padding = layer.op.get_attr('padding')
-                    relevances.append(self.backprop_avg_pool(activation, relevances[-1], ksize, strides, padding))
-                    layer = activation
-                elif 'concat' in layer.name:
-                    activation = self.get_parent(layer, 4)
-                    relevances.append(self.backprop_inception(layer, activation, relevances[-1]))
-                    #print(layer.op.name, activation.op.name)
-                    layer = activation
-                elif 'MaxPool' in layer.name:
-                    activation = self.get_parent(layer, 1)
-                    ksize = layer.op.get_attr('ksize')
-                    strides = layer.op.get_attr('strides')
-                    padding = layer.op.get_attr('padding')
-                    relevances.append(self.backprop_avg_pool(activation, relevances[-1], ksize, strides, padding))
-                    layer = activation
-                elif 'Relu' in layer.name:
-                    activation = self.get_parent(layer, 3)
-                    conv = self.get_parent(layer, 2)
-                    weights = self.get_parent(conv.op.inputs[1], 1)
-                    if not ('ExpandDims' in activation.name):
-                        relevances.append(self.backprop_conv(activation, weights, relevances[-1], strides=conv.op.get_attr('strides')))
-                    else:
-                        relevances.append(self.backprop_conv_input(activation, weights, relevances[-1], strides=conv.op.get_attr('strides')))
-                    layer = activation
-                else:
-                    print('end:', layer.name)
-                    break
-
-        return relevances
+        return traverse_graph(self, layer, relevances)
 
     def get_parent(self, child, num_skips):
         parent = child
