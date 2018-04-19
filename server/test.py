77bdd3dc74a5509d3479a647498fc03055ef3bf9
oleob <ole.barsch@gmail.com>
2018-04-19 11:15:32 +0200

    test vgg_16

--- server/test.py
+++ server/test.py
@@ -3,7 +3,7 @@ from utilities.cleaner import clear_temp_folder
 
 clear_temp_folder()
 
-slim_net = Network('InceptionV1')
+slim_net = Network('vgg_16') #InceptionV1 or vgg_16
 #slim_net.predict()
 #slim_net.print_layers()
 slim_net.deep_taylor()
