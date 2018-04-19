f6ce446759109e9b23b5b221992c5776bfb8a651
oleob <ole.barsch@gmail.com>
2018-04-19 10:18:42 +0200

    Add vgg_16

--- server/utilities/slim_network.py
+++ server/utilities/slim_network.py
@@ -7,8 +7,8 @@ import cv2
 checkpoints_dir = 'checkpoints'
 
 from datasets import imagenet
-from nets import inception
-from preprocessing import inception_preprocessing
+from nets import inception, vgg
+from preprocessing import inception_preprocessing, vgg_preprocessing
 
 from tensorflow.contrib import slim
 
@@ -21,6 +21,7 @@ class Network:
         img = tf.read_file('./static/images/penguins3.jpg')
         image = tf.image.decode_jpeg(img, channels=3)
         if network_name == 'InceptionV1':
+            shift_index = False
             traverse_graph = traverse.inception_v1
             image_size = inception.inception_v1.default_image_size
             processed_image = inception_preprocessing.preprocess_image(image, image_size, image_size, is_training=False)
@@ -32,7 +33,18 @@ class Network:
 
                 init_fn = slim.assign_from_checkpoint_fn(os.path.join(checkpoints_dir, 'inception_v1.ckpt'), slim.get_model_variables('InceptionV1'))
         elif network_name == 'vgg_16':
-            print("potato")
+            shift_index = True
+            traverse_graph = traverse.vgg_16
+            image_size = vgg.vgg_16.default_image_size
+            processed_image = vgg_preprocessing.preprocess_image(image, image_size, image_size, is_training=False)
+            processed_images  = tf.expand_dims(processed_image, 0)
+
+            # Create the model, use the default arg scope to configure the batch norm parameters.
+            with slim.arg_scope(vgg.vgg_arg_scope()):
+                logits, _ = vgg.vgg_16(processed_images, num_classes=1000, is_training=False)
+                probabilities = tf.nn.softmax(logits)
+
+                init_fn = slim.assign_from_checkpoint_fn(os.path.join(checkpoints_dir, 'vgg_16.ckpt'), slim.get_model_variables('vgg_16'))
 
         sess = tf.Session()
         init_fn(sess)
@@ -40,6 +52,7 @@ class Network:
         self.output_layer = probabilities
         self.traverse_graph = traverse_graph
         self.checkpoints_dir = 'checkpoints'
+        self.shift_index = shift_index
 
     def predict(self):
 
@@ -48,9 +61,14 @@ class Network:
         sorted_inds = [i[0] for i in sorted(enumerate(-probabilities), key=lambda x:x[1])]
 
         names = imagenet.create_readable_names_for_imagenet_labels()
+
+        shift = 0
+        if self.shift_index:
+            shift = 1
+
         for i in range(5):
             index = sorted_inds[i]
-            print('Probability %0.2f%% => [%s]' % (probabilities[index] * 100, names[index]))
+            print('Probability %0.2f%% => [%s]' % (probabilities[index] * 100, names[index + shift]))
 
     def print_layers(self):
         layer = self.sess.graph.get_tensor_by_name('Softmax:0') #TODO replace with output_layer
