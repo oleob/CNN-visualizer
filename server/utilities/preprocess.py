32ec89cf241399a2e04168b57f3c1d9cdd17bed3
oleob <ole.barsch@gmail.com>
2018-04-23 16:55:03 +0200

    Undo change to padding

--- server/utilities/preprocess.py
+++ server/utilities/preprocess.py
@@ -3,21 +3,7 @@ import numpy as np
 import math
 
 def pad_image(img):
-    vertical = int((img.shape[1] - img.shape[0]))
-    horizontal= int((img.shape[0] - img.shape[1]))
-    if vertical < 0:
-        top = 0
-        bottom = 0
-    else:
-        top = math.floor(vertical/2)
-        bottom = math.ceil(vertical/2)
-
-    if horizontal < 0:
-        left = 0
-        right = 0
-    else:
-        left = math.floor(horizontal/2)
-        right = math.ceil(horizontal/2)
-
-    padded_img=cv2.copyMakeBorder(img, top=top, bottom=bottom, left=left, right=right, borderType= cv2.BORDER_REPLICATE)
+    bordersize_v = int(np.amax([0.0, (img.shape[0] - img.shape[1])/2]))
+    bordersize_h = int(np.amax([0.0, (img.shape[1] - img.shape[0])/2]))
+    padded_img = cv2.copyMakeBorder(img, top=bordersize_h, bottom=bordersize_h, left=bordersize_v, right=bordersize_v, borderType= cv2.BORDER_REPLICATE)
     return padded_img
