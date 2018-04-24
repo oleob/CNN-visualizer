626130e9638ab820927e43b337950932a58bbe06
oleob <ole.barsch@gmail.com>
2018-04-24 14:45:59 +0200

    Add field for selecting network

--- app/src/components/SettingsDrawer.jsx
+++ app/src/components/SettingsDrawer.jsx
@@ -1,17 +1,72 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Drawer from 'material-ui/Drawer';
 import Button from 'material-ui/Button';
+import { MenuItem } from 'material-ui/Menu';
+import Select from 'material-ui/Select';
+import { FormControl } from 'material-ui/Form';
+import { InputLabel } from 'material-ui/Input';
+import { withStyles } from 'material-ui/styles';
 
+import {changeSettings} from '../utilities/apiCalls';
 
-const SettingsDrawer = (props) => {
-  return (
-    <Drawer open={props.showDrawer} onClose={props.toggleDrawer}>
-        <Button variant="raised">
-          Potato
+const networkNames = ['InceptionV1', 'vgg_16'];
+
+const styles = theme => ({
+  networkName: {
+    width: 'auto',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 30,
+  },
+  saveButton: {
+    width: 'auto',
+    marginLeft: 20,
+    marginRight: 20,
+    marginTop: 15,
+  }
+})
+
+class SettingsDrawer extends Component {
+
+  state = {
+     network_name: networkNames[0],
+   };
+
+  handleChange = event => {
+   this.setState({ [event.target.name]: event.target.value });
+  };
+
+  saveChanges = event => {
+    changeSettings(this.state).then((res)=>{
+      console.log(res)
+    });
+  }
+
+  render(){
+    const { classes } = this.props;
+
+    return (
+      <Drawer className={classes.drawer} open={this.props.showDrawer} onClose={this.props.toggleDrawer}>
+        <form autoComplete="off">
+          <FormControl className={classes.networkName} >
+            <InputLabel htmlFor="controlled-open-select">Model</InputLabel>
+            <Select value={this.state.network_name} onChange={this.handleChange} inputProps={{ name: 'network_name', id: 'controlled-open-select',}}>
+              {
+                networkNames.map((name, i) => (
+                  <MenuItem key={i} value={name}>{name}</MenuItem>
+                ))
+              }
+            </Select>
+          </FormControl>
+        </form>
+        <Button className={classes.saveButton} onClick={this.saveChanges} variant="raised">
+          Save changes
         </Button>
-    </Drawer>
-  );
+      </Drawer>
+    );
+  }
+
 }
 
 
-export default SettingsDrawer;
+export default withStyles(styles)(SettingsDrawer);
