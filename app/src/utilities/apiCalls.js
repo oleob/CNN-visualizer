007e9c990e8693ebeb36ecb06b22587c616518c7
oleob <ole.barsch@gmail.com>
2018-04-25 10:48:39 +0200

    remove unused function

--- app/src/utilities/apiCalls.js
+++ app/src/utilities/apiCalls.js
@@ -12,19 +12,6 @@ const postRequest = (url, body) => {
   });
 }
 
-const changeSettings = (settings) => {
-  return new Promise((resolve, reject) => {
-    axios.post('/change_settings', settings).then((res) => {
-      if(res.data.status === 'ok') {
-        resolve(res.data)
-      }
-      else{
-        reject(res.data)
-      }
-    })
-  })
-}
-
 const activations = (layerName) => {
   return new Promise((resolve, reject) => {
     axios.post('activations', {layer_name: layerName}).then((res) => {
