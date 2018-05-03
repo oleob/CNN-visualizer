bba16a11a84c96f8997766dacb4dfebddbed390a
oleob <ole.barsch@gmail.com>
2018-05-03 15:17:48 +0200

    Add states for containers

--- app/src/containers/Content.jsx
+++ app/src/containers/Content.jsx
@@ -11,13 +11,65 @@ import Activations from './subContainers/Activations';
 
 class Content extends Component {
 
+  state = {
+    global: {
+      networkName: 'InceptionV1',
+    },
+    home: {
+
+    },
+    predict: {
+
+    },
+    visualize: {
+
+    },
+    activations: {
+
+    }
+  }
+
+  updateState = (name, values) => {
+    console.log(name, values)
+    let newState = {};
+    //Map old values to new state
+    Object.entries(this.state[name]).map((item) => newState[item[0]] = item[1]);
+    //map new values to new state
+    Object.entries(values).map((item) => newState[item[0]] = item[1]);
+    this.setState({ [name]: newState });
+  }
+
   render(){
     return(
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/predict" component={Predict} />
-        <Route path="/visualize" component={Visualize} />
-        <Route path="/activations" component={Activations} />
+        <Route exact path="/"
+        render={() => (<Home
+          name="home"
+          updateState={this.updateState}
+          globalState={this.state.global}
+          localState={this.state.home} />)}
+        />
+        <Route path="/predict"
+        render={() => (<Predict
+          name="predict"
+          updateState={this.updateState}
+          globalState={this.state.global}
+          localState={this.state.predict} />)}
+        />
+        <Route path="/visualize"
+        render={() => (<Visualize
+          name="visualize"
+          updateState={this.updateState}
+          globalState={this.state.global}
+          localState={this.state.visualize} />)}
+        />
+        <Route path="/activations"
+        render={() => (<Activations
+          name="activations"
+          updateState={this.updateState}
+          globalState={this.state.global}
+          localState={this.state.activations} />)}
+        />
       </Switch>
     );
   }
