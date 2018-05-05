705b79f8ad746423ef4f40082e7a07eb6fded78c
oleob <ole.barsch@gmail.com>
2018-05-05 10:57:18 +0200

    add settings for deep taylor

---
+++ app/src/components/DeepTaylorSettings.jsx
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import Paper from 'material-ui/Paper';
+import { withStyles } from 'material-ui/styles';
+import Typography from 'material-ui/Typography';
+import { postRequest } from '../utilities/apiCalls';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import { CircularProgress } from 'material-ui/Progress';
+
+const styles = {
+  paper: {
+    display: 'inline-block',
+    padding: 20,
+  },
+  container: {
+    textAlign: 'center',
+    marginTop: 20,
+  },
+  formControl: {
+    minWidth: 120,
+  },
+  buttonContainer: {
+    marginTop: 15,
+  }
+};
+
+class DeepTaylorSettings extends Component {
+
+  state = {
+    loading: false,
+    numFilters: 10,
+    result: {},
+  }
+
+  handleChange = name => event => {
+   this.setState({ [name]: event.target.value });
+  };
+
+  getDecomposition = () => {
+
+    this.setState({loading: true});
+
+    const body = {
+      num_filters: this.state.numFilters,
+    };
+
+    postRequest('/deep_taylor', body).then((res)=>{
+      this.setState({
+        result: res.results,
+        loading: false,
+      })
+    })
+  }
+
+  render() {
+    const { classes } = this.props;
+    return(
+      <div className={classes.container}>
+        <Paper className={classes.paper}>
+          <Typography variant="headline" component="h3">
+            Settings
+          </Typography>
+          <form autoComplete="off">
+            <TextField
+              id="number"
+              label="Number"
+              value={this.state.numFilters}
+              onChange={this.handleChange('numFilters')}
+              type="number"
+              className={classes.textField}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              margin="normal"
+            />
+          </form>
+          <div className={classes.buttonContainer}>
+            {!this.state.loading &&
+              <Button className={classes.saveButton} onClick={this.getDecomposition} variant="raised">
+                Get Decomposition
+              </Button>
+            }
+            {this.state.loading &&
+              <CircularProgress size={68} className={classes.loadingIcon}/>
+            }
+          </div>
+        </Paper>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(DeepTaylorSettings);
