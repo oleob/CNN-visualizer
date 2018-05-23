579c2af7dc5df0e0d3cc87fa7a97bbf298ed882e
oleob <ole.barsch@gmail.com>
2018-05-23 17:10:13 +0200

    save state

--- app/src/containers/subContainers/Visualize.jsx
+++ app/src/containers/subContainers/Visualize.jsx
@@ -3,10 +3,20 @@ import React, { Component } from 'react';
 import FeatureVis from '../../components/FeatureVis';
 
 class Visualize extends Component {
+
+  updateState = (newState) => {
+    this.props.updateState(this.props.name, newState);
+  }
+
   render() {
     return(
       <div className="content">
-        <FeatureVis/>
+        <FeatureVis
+          localState={this.props.localState}
+          globalState={this.props.globalState}
+          updateState={this.updateState}
+          updateGlobalState={this.props.updateGlobalState}
+        />
       </div>
     );
   }
