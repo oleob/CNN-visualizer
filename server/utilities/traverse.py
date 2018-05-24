eed449a7d1e1dcb5316c57084f9a363da4247939
oleob <ole.barsch@gmail.com>
2018-05-24 10:42:53 +0200

    Fix names corresponding to heatmaps

--- server/utilities/traverse.py
+++ server/utilities/traverse.py
@@ -58,38 +58,41 @@ def inception_v1(self, layer, relevances):
 
 def vgg_16 (self, layer, relevances):
     while not ('ExpandDims' in layer.name):
-        with tf.variable_scope(layer.op.name.split('/')[-2] + '_taylor'):
-            if 'squeezed' in layer.name:
-                layer = self.get_parent(layer, 1)
-            elif 'BiasAdd' in layer.name:
-                conv = self.get_parent(layer,1)
-                weights = self.get_parent(conv.op.inputs[1], 1)
-                activation = self.get_parent(layer, 2)
-                strides = conv.op.get_attr('strides')
-                padding = conv.op.get_attr('padding')
+        if 'squeezed' in layer.name:
+            layer = self.get_parent(layer, 1)
+        elif 'BiasAdd' in layer.name:
+            conv = self.get_parent(layer,1)
+            weights = self.get_parent(conv.op.inputs[1], 1)
+            activation = self.get_parent(layer, 2)
+            strides = conv.op.get_attr('strides')
+            padding = conv.op.get_attr('padding')
+            with tf.variable_scope(activation.op.name.split('/')[-2] + '_taylor'):
                 relevances.append(self.backprop_conv(activation, weights, relevances[-1], strides, padding))
-                layer = activation
-            elif 'dropout' in layer.name:
-                layer = self.get_parent(layer, 1)
-            elif 'Relu' in layer.name:
-                conv = self.get_parent(layer,2)
-                weights = self.get_parent(conv.op.inputs[1], 1)
-                activation = self.get_parent(layer, 3)
-                strides = conv.op.get_attr('strides')
-                padding = conv.op.get_attr('padding')
-                if not ('ExpandDims' in activation.name):
+            layer = activation
+        elif 'dropout' in layer.name:
+            layer = self.get_parent(layer, 1)
+        elif 'Relu' in layer.name:
+            conv = self.get_parent(layer,2)
+            weights = self.get_parent(conv.op.inputs[1], 1)
+            activation = self.get_parent(layer, 3)
+            strides = conv.op.get_attr('strides')
+            padding = conv.op.get_attr('padding')
+            if not ('ExpandDims' in activation.name):
+                with tf.variable_scope(activation.op.name.split('/')[-2] + '_taylor'):
                     relevances.append(self.backprop_conv(activation, weights, relevances[-1], strides, padding))
-                else:
-                    relevances.append(self.backprop_conv_input(activation, weights, relevances[-1], strides, padding))
-                layer = activation
-            elif 'MaxPool' in layer.name:
-                activation = self.get_parent(layer, 1)
-                ksize = layer.op.get_attr('ksize')
-                strides = layer.op.get_attr('strides')
-                padding = layer.op.get_attr('padding')
-                relevances.append(self.backprop_avg_pool(activation, relevances[-1], ksize, strides, padding))
-                layer = activation
             else:
-                print('end', layer.name)
-                break
+                with tf.variable_scope(activation.op.name + '_taylor'):
+                    relevances.append(self.backprop_conv_input(activation, weights, relevances[-1], strides, padding))
+            layer = activation
+        elif 'MaxPool' in layer.name:
+            activation = self.get_parent(layer, 1)
+            ksize = layer.op.get_attr('ksize')
+            strides = layer.op.get_attr('strides')
+            padding = layer.op.get_attr('padding')
+            with tf.variable_scope(activation.op.name.split('/')[-2] + '_taylor'):
+                relevances.append(self.backprop_avg_pool(activation, relevances[-1], ksize, strides, padding))
+            layer = activation
+        else:
+            print('end', layer.name)
+            break
     return relevances
